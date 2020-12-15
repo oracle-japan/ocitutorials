6f78a80588fc9a66f2a93f8f34c95b438870039e
mmarukaw <mmarukaw@users.noreply.github.com>
2020-12-15 00:46:43 +0000

    deploy: 056f3524445831a27c1c119222367bf768b490e4

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,25 +1,181 @@
 var store = [{
-        "title": "OCIコンソールにアクセスして基本を理解する",
-        "excerpt":"Oracle Cloud Infrastructure を使い始めるにあたって、コンソール画面にアクセスし、ログインを行います。 また、Oracle Cloud Infrastructure のサービスを利用するのにあたって必要なサービス・リミット、コンパートメントやポリシーなどのIAMリソースおよびリージョンについて、コンセプトをコンソール画面の操作を通じて学習し、理解します。 所要時間 : 約25分 前提条件 : 有効な Oracle Cloud Infrastructure のテナントと、アクセスのための有効なユーザーIDとパスワードがあること 無償トライアル環境のお申込みについては こちら の資料を参照してください。 注意 : チュートリアル内の画面ショットについては Oracle Cloud Infrastructure の現在のコンソール画面と異なっている場合があります image サポートされるブラウザの確認 このチュートリアルでは、Oracle Cloud Infrastructure のコンソール画面からの操作を中心に作業を行います。 サポートされるブラウザを確認し、いずれかのブラウザをローカル環境にインストールしてください。 ログイン情報の確認 コンソールにアクセスするにあたり、ログイン情報の入力が必要になります。ログイン情報には以下のものが含まれます。 テナント名(クラウド・アカウント名) - Oracle Cloud Infrastructure を契約したり、トライアル環境を申し込んだ際に払い出される一意のID ユーザー名 - ログインのためのユーザー名 パスワード - ログインのためのパスワード ログイン情報の入手方法は、ユーザーが作られるタイミングによって異なります。...","categories": ["Services","Compute"],
-        "tags": ["スライドあり","レベル:応用(200)"],
-        "url": "https://oracle-japan.github.io/ocitutorials/beginners/01-getting-started/",
+        "title": "Oracle Container Engine for Kubernetes(OKE)でKubernetesを始めてみよう",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/advanced-cloudnative/oke-for-beginners/",
         "teaser": null
       },{
-        "title": "チュートリアル - オブジェクト・ストレージを使う",
-        "excerpt":"Oracle Cloud Infrastructureオブジェクト・ストレージ・サービスは、高い信頼性と高い費用対効果を両立するスケーラブルなクラウドストレージです。 オブジェクト・ストレージを利用すると、分析用のビッグ・データや、イメージやビデオ等のリッチ・メディア・コンテンツなど、あらゆるコンテンツ・タイプの非構造化データを無制限に保管できます。 オブジェクト・ストレージはリージョン単位のサービスで、コンピュート・インスタンスからは独立して動作します。 ユーザーはオブジェクト・ストレージのエンドポイントに対し、OCIの内部、外部を問わずどこからでもアクセスすることができます。 OCIのIdentity and Access Management(IAM)機能をを利用した適切なアクセスコントロールや、リソース・リミットを設定することも可能です。 この章では、コンソール画面からオブジェクト・ストレージにアクセスし、スタンダード・バケットの作成やオブジェクトのアップロード、ダウンロードなどの基本的な操作、また事前認証リクエストを作成して一般ユーザー向けにダウンロードリンクを生成する手順について学習します。 所要時間 : 15分 前提条件 : 適切なコンパートメント(ルート・コンパートメントでもOKです)と、そこに対する適切なオブジェクト・ストレージの管理権限がユーザーに付与されていること 注意 : チュートリアル内の画面ショットについてはOracle Cloud Infrastructureの現在のコンソール画面と異なっている場合があります 1. コンソール画面の確認とバケットの作成 オブジェクト・ストレージ・サービスにおいて、バケットはオブジェクトを格納する箱として機能します。 バケットはコンパートメントに紐付ける必要があり、バケットおよびその中のオブジェクトに対する操作に関する権限は、コンパートメントのポリシーを通じて制御します。 まず、コンソール画面からバケットを作成していきます。 コンソールメニューから ストレージ → オブジェクト・ストレージ を選択し、バケットの作成 ボタンを押します 立ち上がった バケットの作成 ウィンドウに以下の項目を入力し、バケットの作成 ボタンを押します。指定がないものは任意の値でOKです。 バケット名 - 任意の文字列 (画面では TutorialBucket と入力しています) ストレージ層 - 標準 を選択 オブジェクト・バージョニングの有効化 - チェックなし...","categories": ["Services","Compute"],
-        "tags": ["スライドあり","レベル:応用(200)"],
-        "url": "https://oracle-japan.github.io/ocitutorials/beginners/07-object-storage/",
+        "title": "KubernetesでサンプルアプリケーションのデプロイとCI/CDを体験してみよう",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/advanced-cloudnative/oke-for-intermediates/",
         "teaser": null
       },{
-        "title": "OCIコンソールにアクセスして基本を理解する",
-        "excerpt":"Oracle Cloud Infrastructure を使い始めるにあたって、コンソール画面にアクセスし、ログインを行います。 また、Oracle Cloud Infrastructure のサービスを利用するのにあたって必要なサービス・リミット、コンパートメントやポリシーなどのIAMリソースおよびリージョンについて、コンセプトをコンソール画面の操作を通じて学習し、理解します。 所要時間 : 約25分 前提条件 : 有効な Oracle Cloud Infrastructure のテナントと、アクセスのための有効なユーザーIDとパスワードがあること 無償トライアル環境のお申込みについては こちら の資料を参照してください。 注意 : チュートリアル内の画面ショットについては Oracle Cloud Infrastructure の現在のコンソール画面と異なっている場合があります image サポートされるブラウザの確認 このチュートリアルでは、Oracle Cloud Infrastructure のコンソール画面からの操作を中心に作業を行います。 サポートされるブラウザを確認し、いずれかのブラウザをローカル環境にインストールしてください。 ログイン情報の確認 コンソールにアクセスするにあたり、ログイン情報の入力が必要になります。ログイン情報には以下のものが含まれます。 テナント名(クラウド・アカウント名) - Oracle Cloud Infrastructure を契約したり、トライアル環境を申し込んだ際に払い出される一意のID ユーザー名 - ログインのためのユーザー名 パスワード - ログインのためのパスワード ログイン情報の入手方法は、ユーザーが作られるタイミングによって異なります。...","categories": ["Services","Compute"],
-        "tags": ["スライドあり","レベル:応用(200)"],
-        "url": "https://oracle-japan.github.io/ocitutorials/prerequisites/01-Getting-Started/",
+        "title": "Oracle Functions ハンズオン（ビギナー向け）",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/advanced-cloudnative/functions-for-beginners/",
         "teaser": null
       },{
-        "title": "自動スケーリング機能をさわってみよう",
-        "excerpt":"自動スケーリング機能 (Auto Scaling) の解説資料です。2019年8月27日のOCIスキルアップセミナーで使用しました。  OCIの自動スケーリング機能と、それを支えるコンポーネント(インスタンス構成、インスタンス・プール、自動スケーリング構成)を実際に触って動作を理解します。   スライド          ","categories": ["Services","Compute"],
-        "tags": ["スライドあり","レベル:応用(200)"],
-        "url": "https://oracle-japan.github.io/ocitutorials/prerequisites/autoscaling/",
-        "teaser": "https://speakerd.s3.amazonaws.com/presentations/3277a22d639b4857b185569a6181278c/slide_0.jpg"
+        "title": "Oracle Cloud Infrastructure API Gateway ハンズオン（ビギナー向け）",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/advanced-cloudnative/apigw-for-beginners/",
+        "teaser": null
+      },{
+        "title": "クラウドでOracle Exadata を使う",
+        "excerpt":" ","categories": [],
+        "tags": ["https://community.oracle.com/docs/DOC-1038411"],
+        "url": "https://oracle-japan.github.io/ocitutorials/advanced-enterprise/using-oracle-exadata/",
+        "teaser": null
+      },{
+        "title": "その1 - OCIコンソールにアクセスして基本を理解する",
+        "excerpt":"Oracle Cloud Infrastructure を使い始めるにあたって、コンソール画面にアクセスし、ログインを行います。 また、Oracle Cloud Infrastructure のサービスを利用するのにあたって必要なサービス・リミット、コンパートメントやポリシーなどのIAMリソースおよびリージョンについて、コンセプトをコンソール画面の操作を通じて学習し、理解します。 所要時間 : 約25分 前提条件 : 有効な Oracle Cloud Infrastructure のテナントと、アクセスのための有効なユーザーIDとパスワードがあること 無償トライアル環境のお申込みについては こちら の資料を参照してください。 注意 : チュートリアル内の画面ショットについては Oracle Cloud Infrastructure の現在のコンソール画面と異なっている場合があります image サポートされるブラウザの確認 このチュートリアルでは、Oracle Cloud Infrastructure のコンソール画面からの操作を中心に作業を行います。 サポートされるブラウザを確認し、いずれかのブラウザをローカル環境にインストールしてください。 ログイン情報の確認 コンソールにアクセスするにあたり、ログイン情報の入力が必要になります。ログイン情報には以下のものが含まれます。 テナント名(クラウド・アカウント名) - Oracle Cloud Infrastructure を契約したり、トライアル環境を申し込んだ際に払い出される一意のID ユーザー名 - ログインのためのユーザー名 パスワード - ログインのためのパスワード ログイン情報の入手方法は、ユーザーが作られるタイミングによって異なります。...","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/advanced-webapp/01-getting-started/",
+        "teaser": null
+      },{
+        "title": "その1 - OCIコンソールにアクセスして基本を理解する",
+        "excerpt":"Oracle Cloud Infrastructure を使い始めるにあたって、コンソール画面にアクセスし、ログインを行います。 また、Oracle Cloud Infrastructure のサービスを利用するのにあたって必要なサービス・リミット、コンパートメントやポリシーなどのIAMリソースおよびリージョンについて、コンセプトをコンソール画面の操作を通じて学習し、理解します。 所要時間 : 約25分 前提条件 : 有効な Oracle Cloud Infrastructure のテナントと、アクセスのための有効なユーザーIDとパスワードがあること 注意 : チュートリアル内の画面ショットについては Oracle Cloud Infrastructure の現在のコンソール画面と異なっている場合があります。 1. サポートされるブラウザの確認 このチュートリアルでは、Oracle Cloud Infrastructure のコンソール画面からの操作を中心に作業を行います。 サポートされるブラウザを確認し、いずれかのブラウザをローカル環境にインストールしてください。 2. ログイン情報の確認 コンソールにアクセスするにあたり、ログイン情報の入力が必要になります。ログイン情報には以下のものが含まれます。 テナント名(クラウド・アカウント名) - Oracle Cloud Infrastructure を契約したり、トライアル環境を申し込んだ際に払い出される一意のID ユーザー名 - ログインのためのユーザー名 パスワード - ログインのためのパスワード ログイン情報の入手方法は、ユーザーが作られるタイミングによって異なります。 テナントの最初のユーザーの場合 テナントの最初の管理者ユーザーに対しては、テナントが作成された際にシステムから通知メールが送付されています。...","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/beginners/getting-started/",
+        "teaser": "https://oracle-japan.github.io/ocitutorials/beginners/getting-started/img2.png"
+      },{
+        "title": "その2 - クラウドに仮想ネットワーク(VCN)を作る",
+        "excerpt":"Oracle Cloud Infrastructure を利用するにあたっての最初のステップは、仮想クラウド・ネットワーク(Virtual Cloud Network : VCN) を作成することです。ネットワークの管理者が最初に仮想ネットワークを作ることで、その後インスタンスの管理者やストレージの管理者が、作成した仮想ネットワークの構成やルールに従ってコンポーネントを配置することができるようになります。 このチュートリアルでは、コンソール画面から仮想クラウド・ネットワーク(VCN)を1つ作成し、その構成について確認してくことで、OCIのネットワークに対する理解を深めます。 所要時間 : 約15分 前提条件 : Oracle Cloud Infrastructure の環境(無料トライアルでも可) と、管理権限を持つユーザーアカウントがあること OCIコンソールにアクセスして基本を理解する - Oracle Cloud Infrastructureを使ってみよう(その1)を完了していること 注意 : チュートリアル内の画面ショットについては Oracle Cloud Infrastructure の現在のコンソール画面と異なっている場合があります 目次： 1. 仮想クラウドネットワーク(VCN)とは? 2. 仮想クラウド・ネットワークの作成 3. 作成した仮想クラウド・ネットワークの確認 1. 仮想クラウドネットワーク(VCN)とは? 仮想クラウド・ネットワークの作成は、基本的にはオンプレミスでよく利用されるネットワークの各コンポーネントの仮想バージョンをクラウド上で構築していくようなものをイメージしていただくとわかりやすいと思います。 仮想クラウド・ネットワークは以下のようなコンポーネントから構成されます。 サブネット - 仮想ネットワークを構成するIPv4のサブネット 仮想NIC - インスタンスにアタッチされ、サブネットに接続する仮想的なネットワーク・インターフェース・カード プライベートIPアドレス...","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/beginners/creating-vcn/",
+        "teaser": "https://oracle-japan.github.io/ocitutorials/beginners/creating-vcn/img4.png"
+      },{
+        "title": "その3 - インスタンスを作成する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/beginners/creating-compute-instance/",
+        "teaser": null
+      },{
+        "title": "その4 - ブロック・ボリュームをインスタンスにアタッチする",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/beginners/attaching-block-volume/",
+        "teaser": null
+      },{
+        "title": "その5 - インスタンスのライフサイクルを管理する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/beginners/managing-instance-lifecycle/",
+        "teaser": null
+      },{
+        "title": "その6 - ファイルストレージサービス(FSS)で共有ネットワークボリュームを利用する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/beginners/using-file-storage/",
+        "teaser": null
+      },{
+        "title": "その7 - オブジェクト・ストレージを使う",
+        "excerpt":"チュートリアル一覧に戻る : 入門編 - Oracle Cloud Infrastructure を使ってみよう Oracle Cloud Infrastructureオブジェクト・ストレージ・サービスは、高い信頼性と高い費用対効果を両立するスケーラブルなクラウドストレージです。 オブジェクト・ストレージを利用すると、分析用のビッグ・データや、イメージやビデオ等のリッチ・メディア・コンテンツなど、あらゆるコンテンツ・タイプの非構造化データを無制限に保管できます。 オブジェクト・ストレージはリージョン単位のサービスで、コンピュート・インスタンスからは独立して動作します。 ユーザーはオブジェクト・ストレージのエンドポイントに対し、OCIの内部、外部を問わずどこからでもアクセスすることができます。 OCIのIdentity and Access Management(IAM)機能をを利用した適切なアクセスコントロールや、リソース・リミットを設定することも可能です。 この章では、コンソール画面からオブジェクト・ストレージにアクセスし、スタンダード・バケットの作成やオブジェクトのアップロード、ダウンロードなどの基本的な操作、また事前認証リクエストを作成して一般ユーザー向けにダウンロードリンクを生成する手順について学習します。 所要時間 : 15分 前提条件 : 適切なコンパートメント(ルート・コンパートメントでもOKです)と、そこに対する適切なオブジェクト・ストレージの管理権限がユーザーに付与されていること 注意 : チュートリアル内の画面ショットについてはOracle Cloud Infrastructureの現在のコンソール画面と異なっている場合があります 1. コンソール画面の確認とバケットの作成 オブジェクト・ストレージ・サービスにおいて、バケットはオブジェクトを格納する箱として機能します。 バケットはコンパートメントに紐付ける必要があり、バケットおよびその中のオブジェクトに対する操作に関する権限は、コンパートメントのポリシーを通じて制御します。 まず、コンソール画面からバケットを作成していきます。 コンソールメニューから ストレージ → オブジェクト・ストレージ を選択し、バケットの作成 ボタンを押します 立ち上がった バケットの作成 ウィンドウに以下の項目を入力し、バケットの作成 ボタンを押します。指定がないものは任意の値でOKです。 バケット名 - 任意の文字列 (画面では TutorialBucket...","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/beginners/object-storage/",
+        "teaser": "https://oracle-japan.github.io/ocitutorials/beginners/object-storage/001.webp"
+      },{
+        "title": "その8 - クラウドでOracle Databaseを使う",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/beginners/using-oracle-database/",
+        "teaser": null
+      },{
+        "title": "モニタリング機能でOCIのリソースを監視する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/monitoring-resources/",
+        "teaser": null
+      },{
+        "title": "ロードバランサーでWebサーバーを負荷分散する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/using-load-balancer/",
+        "teaser": null
+      },{
+        "title": "インスタンスのオートスケーリングを設定する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/autoscaling/",
+        "teaser": null
+      },{
+        "title": "ブロック・ボリュームをバックアップする",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/taking-block-volume-backups/",
+        "teaser": null
+      },{
+        "title": "DNSサービスを使う",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/using-dns/",
+        "teaser": null
+      },{
+        "title": "Email Deliveryを利用した外部へのメール送信(その1　配信環境構築編)",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/sending-emails-1/",
+        "teaser": null
+      },{
+        "title": "GPUインスタンスでディープラーニング",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/deep-learning-with-gpu/",
+        "teaser": null
+      },{
+        "title": "シリアル・コンソールでsshできないインスタンスのトラブルシュートをする",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。   ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/accessing-serial-console/",
+        "teaser": null
+      },{
+        "title": "インスタンスにセカンダリIPを付与する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/attaching-secondary-ips/",
+        "teaser": null
+      },{
+        "title": "コマンドライン(CLI)でOCIを操作する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/using-cli/",
+        "teaser": null
+      },{
+        "title": "TerraformでOCIの構築を自動化する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/terraforming/",
+        "teaser": null
+      },{
+        "title": "OCI Valut (OCI Key Management) でBYOKをする",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/byok-with-vault/",
+        "teaser": null
+      },{
+        "title": "Web Application Firewall(WAF)を使ってWebサーバを保護する",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/protecting-web-servers-with-waf/",
+        "teaser": null
+      },{
+        "title": "Oracle Management Cloud チュートリアルまとめ",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/management-cloud-tutorials/",
+        "teaser": null
+      },{
+        "title": "Oracle Data Safe チュートリアルまとめ",
+        "excerpt":"このチュートリアルは外部のページで紹介しています。リンク先のページをご覧ください。  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/intermediates/data-safe-tutorials/",
+        "teaser": null
+      },{
+        "title": "Oracle Cloud 無料トライアルを申し込む",
+        "excerpt":"Oracle Cloud のほとんどのサービスが利用できるトライアル環境を取得することができます。このチュートリアルの内容を試すのに必要になりますので、まずは取得してみましょう。 ※認証のためにSMSが受け取れる電話とクレジット・カードが必要です(希望しない限り課金はされませんのでご安心を!!)      Oracle Cloud 無料トライアル サインアップガイド   Oracle Cloud Free Tierに関するFAQ  ","categories": [],
+        "tags": [],
+        "url": "https://oracle-japan.github.io/ocitutorials/prerequisites/acquiring-free-trial/",
+        "teaser": null
       }]
