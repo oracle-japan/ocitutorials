5b96a21a519c81112ce378fa348f415099f6d512
omomoki <omomoki@users.noreply.github.com>
2024-06-11 08:51:04 +0000

    deploy: 0973ece34af3f4ea47cee501e8e081447dfc17d2

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -365,11 +365,11 @@ var store = [{
         "url": "/ocitutorials/basedb/dbcs201-pdb-plug/",
         "teaser": "/ocitutorials/basedb/dbcs201-pdb-plug/pdb-plug05.png"
       },{
-        "title": "202:DBMS_CLOUDを使ってObject StorageからBaseDBにデータを移行しよう ",
-        "excerpt":"はじめに DBMS_CLOUDはオブジェクト・ストレージのデータを操作するための包括的なサポートを提供するPL/SQLパッケージです。 DBMS_CLOUDはAutonomous Database (ADB) に実装されているPL/SQLパッケージですが、手動インストールすることでBaseDBでも利用可能です。 ADBでDBMS_CLOUDを利用する方法は202: コマンドラインから大量データをロードしてみよう(DBMS_CLOUD)で学ぶことができます。 ここでは、DBMS_CLOUDパッケージを利用してObject StorageのデータをBase Database Service (BaseDB)へ取り込む手順をご紹介します。　　 このチュートリアルで実行する内容のイメージは以下の通りです。 前提条件 : Oracle Database 19.9以上 もしくは　Oracle Database 21.3以上 101: Oracle Cloud で Oracle Database を使おう を通じて Oracle Database の作成が完了していること 以下にリンクされているサンプルデータのCSVファイルをダウンロードしていること サンプルデータファイルのダウンロードリンク その7 - オブジェクト・ストレージを使う を通じてバケットの作成・データファイル(CSV)のアップロードが完了していること 目次 1. 事前準備 2. DBMS_CLOUD PL/SQLパッケージのダウンロード 3. Walletの作成 4. Walletの場所の設定 5....","categories": [],
+        "title": "202:DBMS_CLOUDを使ってObject StorageのデータをBaseDBから参照しよう",
+        "excerpt":"はじめに DBMS_CLOUDはオブジェクト・ストレージのデータを操作するための包括的なサポートを提供するPL/SQLパッケージです。 DBMS_CLOUDはAutonomous Database (ADB) に実装されているPL/SQLパッケージですが、手動インストールすることでBaseDBでも利用可能です。 ADBでDBMS_CLOUDを利用する方法は202: コマンドラインから大量データをロードしてみよう(DBMS_CLOUD)で学ぶことができます。 ここでは、DBMS_CLOUDパッケージを利用してObject StorageのデータをBase Database Service (BaseDB)から外部表として参照する手順をご紹介します。　　 このチュートリアルで実行する内容のイメージは以下の通りです。 前提条件 : Oracle Database 19.9以上 もしくは　Oracle Database 21.3以上 PDBにユーザーが作成されていて、そのユーザーに接続可能であること 101: Oracle Cloud で Oracle Database を使おう を通じて Oracle Database の作成が完了していること 以下にリンクされているサンプルデータのCSVファイルをダウンロードしていること サンプルデータファイルのダウンロードリンク その7 - オブジェクト・ストレージを使う を通じてバケットの作成・データファイル(CSV)のアップロードが完了していること 目次 1. 事前準備 2. DBMS_CLOUD PL/SQLパッケージのダウンロード 3. Walletの作成 4. Walletの場所の設定...","categories": [],
         "tags": [],
         "url": "/ocitutorials/basedb/dbcs202-dbms-cloud/",
-        "teaser": "/ocitutorials/basedb/dbcs202-dbms-cloud/dbms-cloud01.png"
+        "teaser": "/ocitutorials/basedb/dbcs202-dbms-cloud/External-table.png"
       },{
         "title": "準備 - Oracle Cloud 無料トライアルを申し込む",
         "excerpt":"Oracle Cloud のほとんどのサービスが利用できるトライアル環境を取得することができます。このチュートリアルの内容を試すのに必要になりますので、まずは取得してみましょう。 ※認証のためにSMSが受け取れる電話とクレジット・カードが必要です(希望しない限り課金はされませんのでご安心を!!)      Oracle Cloud 無料トライアル サインアップガイド   Oracle Cloud Free Tierに関するFAQ  ","categories": [],
